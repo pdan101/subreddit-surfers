ab6159f6d303ba1135a1edde9533d1c92cc73442
Lauren Greenhill <ljg227@cornell.edu>
2021-10-11 18:29:14 -0400

    create units function

--- src/wordProcessor.ml
+++ src/wordProcessor.ml
@@ -25,4 +25,31 @@ let parse (text : string) =
   |> List.map (fun x -> String.trim x)
   |> List.map (fun x -> remove_punc x)
 
+let vowels = "aeiouy"
+
+let consonants = "bcdfghjklmnpqrstwxz"
+
+let check_same type_char word =
+  if
+    String.contains type_char (String.get word 0)
+    && String.contains type_char (String.get word 1)
+  then true
+  else false
+
+let get_tail num_remove word =
+  String.sub word num_remove (String.length word - num_remove)
+
+let rec create_units (word : string) =
+  if String.length word >= 2 then
+    if check_same vowels word then 'V' :: create_units (get_tail 2 word)
+    else if check_same consonants word then
+      'C' :: create_units (get_tail 2 word)
+    else if String.contains vowels (String.get word 0) then
+      'V' :: create_units (get_tail 1 word)
+    else 'C' :: create_units (get_tail 1 word)
+  else if String.length word = 1 then
+    if String.contains vowels (String.get word 0) then [ 'V' ]
+    else [ 'C' ]
+  else []
+
 let stem (word : string) = raise (Failure "Unimplemented")
