49877e33ca2f65846879efcfa7347287e7dbb23f
Lauren Greenhill <ljg227@cornell.edu>
2021-10-12 17:39:32 -0400

    added install and autho documentation

---
+++ src/author.mli
@@ -0,0 +1,9 @@
+(** CS 3110 Final Project
+
+    @author Cella Kove (CMK295)
+    @author Lauren Greenhill (LJG227)
+    @author Prithwish Dan (PD337)
+    @author Sana Gaya (SRG276) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours the team worked on MS1. *)
