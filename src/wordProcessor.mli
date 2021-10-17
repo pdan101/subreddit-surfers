92bb470bde3355389ec2511fb146b9a55a50551a
pdan101 <61089045+pdan101@users.noreply.github.com>
2021-10-17 11:30:29 -0400

    Revert "Stemmer map"

--- src/wordProcessor.mli
+++ src/wordProcessor.mli
@@ -1,23 +1,10 @@
 (**This module contains all the word processing functions for text*)
 
-type stemmed_word = {
-  original_word : string;
-  units : string;
-  num_vcs : int;
-  stemmed : string;
-}
+type word_data
 (**the abstract type containing data about a word*)
 
 type vocabulary
 (**the abstract type containing all the words seen so far*)
-
-type text_block = {
-  original_text : string;
-  stemmed_text : string;
-  stop_words_removed : string;
-  parsed_words : string list;
-  parsed_sentences : string list;
-}
 
 val parse : string -> string list
 (**[parse text] is the list of words contained in text excluding
@@ -39,16 +26,10 @@ val remove_past_participles : string -> int -> string
    there is at least one VC pair), ED (if this leaves at least one vowel
    in the stem), ING (if this leaves at leat one vowel in the stem)*)
 
-val stemmer : string -> stemmed_word
+val stem : string -> string
 (**[stem word] is the stemmed version of word*)
 
 val parse_sentence : string -> string list
 (**[parse text] is the list of words contained in text exlcuding
    trailing whitespace and punctuation Requires: text is in English and
    follows English grammar rules*)
-
-val process_sentence : string -> string
-(**[processed_sentence sentence] is the stemmed version of sentence*)
-
-val remove_stop_words : string list -> string list
-(**[remove_stop_words words] is words with stop words removed*)
