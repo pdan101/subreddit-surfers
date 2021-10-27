064f8a30b908aca5608e20bd2177bfffffa4c239
Lauren Greenhill <ljg227@cornell.edu>
2021-10-27 17:18:11 -0400

    fixed end_cvc to cover fail

--- src/wordProcessor.ml
+++ src/wordProcessor.ml
@@ -112,8 +112,8 @@ let end_cvc word =
     let last_three = get_last units 3 in
     let last_letter = get_last word 1 in
     if
-      (last_three = "CVC" && last_letter <> "w")
-      || last_letter <> "x" || last_letter <> "y"
+      last_three = "CVC" && last_letter <> "w" && last_letter <> "x"
+      && last_letter <> "y"
     then true
     else false
   else false
@@ -126,7 +126,7 @@ let double_consonant word =
 
 let finalize_plurals_past_participles word num_vc =
   let len = String.length word in
-  if len = 3 && end_cvc word && num_vc = 1 then word ^ "e"
+  if end_cvc word && num_vc = 1 then word ^ "e"
   else if len >= 2 then
     let last_two = get_last word 2 in
     if last_two = "at" || last_two = "bl" || last_two = "iz" then
