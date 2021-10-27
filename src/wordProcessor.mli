bf92d803500e5fa63b3be425cf66abb1fed1c0da
Lauren Greenhill <ljg227@cornell.edu>
2021-10-27 17:36:54 -0400

    added documentation

--- src/wordProcessor.mli
+++ src/wordProcessor.mli
@@ -42,8 +42,8 @@ val stemmed_text_block : text_block -> string
 (**[stemmed_text_block] is the stemmed text of text_block*)
 
 val create_units : string -> string
-(**[create_units] is the string with all vowels and consonants grouped
-   and summarized with either V or C*)
+(**[create_units] has all vowels and consonants summarized with either V
+   or C*)
 
 val calc_vc : string -> int
 (**[calc_vc] is the number of VC pairs. This is the same as m in the
@@ -65,7 +65,19 @@ val finalize_plurals_past_participles : string -> int -> string
    only be used if remove_plurals or remove_past_participles has altered
    the word*)
 
-val end_cvc : string -> bool
-(***)
-
 val create_simplified_units : string -> string -> string
+(**[create_simplified_units] has all vowels and consonants grouped
+   together and represented with either V or C*)
+
+val fix_y : string -> string
+(**[fix_y] adds and "i" to a stemmed word if it contains a vowel. This
+   is Step 1c in the porter stemmer paper*)
+
+val remove_e : string -> string
+(**[remove_e] adds "e" to the end of a stemmed word if the number of
+   VC's is greater than 1 or the number of VC's is 1 and does not end
+   with CVC. This is Step 5a in the porter stemmer paper*)
+
+val check_double_consonant : string -> string
+(**[check_double_consonant] removes double consonants from the end of
+   stemmed words if the number VC's is greater than 1*)
