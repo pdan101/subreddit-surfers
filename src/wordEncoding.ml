e161e4e8e1a9ffd0089ac65e2ec4dd69c27fa740
Cella Kove <cmk295@cornell.edu>
2021-10-29 01:27:52 -0400

    Implemented create_encoded_matrix

--- src/wordEncoding.ml
+++ src/wordEncoding.ml
@@ -1,6 +1,6 @@
 open WordProcessor
 open Intake
-open Yojson.Basic
+open Yojson.Basic.Util
 
 let subreddit_json_to_words subreddit_json : string list =
   let subreddit = from_json subreddit_json in
@@ -18,16 +18,16 @@ let subreddit_json_to_stemmed_words subreddit_json : string list =
 let rec write_words_to_json (file : out_channel) (words : string list) :
     unit =
   match words with
-  | [] -> output_string file "}"
+  | [] -> output_string file "\t}\n}"
   | [ h ] ->
-      output_string file ("\"" ^ h ^ "\":\"\"\n");
+      output_string file ("\t\t\"" ^ h ^ "\":\"\"\n");
       write_words_to_json file []
   | h :: t ->
       if pos_out file < 1 then (
-        output_string file "{\n";
+        output_string file "{\n\t\"words\": {\n";
         write_words_to_json file (h :: t))
       else (
-        output_string file ("\"" ^ h ^ "\":\"\",\n");
+        output_string file ("\t\t\"" ^ h ^ "\":\"\",\n");
         write_words_to_json file t)
 
 let subreddit_json_to_word_json processor_function subreddit_json : unit
@@ -38,3 +38,29 @@ let subreddit_json_to_word_json processor_function subreddit_json : unit
   let filepath = "data/subredditVocabJsons/" ^ filename ^ ".json" in
   let file = open_out filepath in
   write_words_to_json file words
+
+let word_json_to_array (word_json : Yojson.Basic.t) : string array =
+  word_json |> member "words" |> to_list |> List.map to_string
+  |> Array.of_list
+
+let rec find_index (current_index : int) (x : 'a) (arr : 'a array) : int
+    =
+  if x = arr.(current_index) then current_index
+  else find_index (current_index + 1) x arr
+
+let create_encoded_matrix (word_json : Yojson.Basic.t) (post : string) :
+    int array array =
+  let vocab_array = word_json_to_array word_json in
+  let post_array = Array.of_list (parse post) in
+  let matrix =
+    Array.make_matrix
+      (Array.length post_array)
+      (Array.length vocab_array)
+      0
+  in
+  Array.iteri
+    (fun index x ->
+      let word_index = find_index 0 x vocab_array in
+      matrix.(index).(word_index) <- 1)
+    post_array;
+  matrix
