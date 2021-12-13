dc4b648638cd1e32cf788ea726565b64c1625625
Prithwish Dan <pd337@cornell.edu>
2021-12-13 15:06:51 -0500

    Fixed some make docs stuff

--- src/customRegression.mli
+++ src/customRegression.mli
@@ -6,7 +6,9 @@ type regression =
   | LASSO
   | Logistic
   | SVM
-  | OLS  (**the type containing all possible kinds of regression*)
+  | OLS
+
+(**the type containing all possible kinds of regression*)
 
 type train_test_data = {
   features_training : Owl.Mat.mat;
@@ -17,8 +19,9 @@ type train_test_data = {
 (**the type containing all relevant sections of the encoded matrix*)
 
 val get_weights : int array list -> float -> regression -> float array
-(**[get_weights encoded_subreddt percent_training regression_type] is
-   resulting weights of the regression*)
+(**[get_weights encoded_subreddit percent_training regression_type] is
+   resulting weights of applying the [regression_type] to the
+   [encoded_subreddit]*)
 
 val get_training_data : Owl.Mat.mat -> float -> train_test_data
 (**[get_training_data encoded_matrix percent_training] is a record
@@ -26,11 +29,14 @@ val get_training_data : Owl.Mat.mat -> float -> train_test_data
    matrix)*)
 
 val create_matrix : int array list -> Owl.Mat.mat
-(**[create_matrix encoded_subreddit] is the encoded subreddit in an Owl
-   matrix*)
+(**[create_matrix encoded_subreddit] is the [encoded_subreddit] in an
+   Owl matrix*)
 
 val calc_upvotes : Owl.Mat.mat -> float array -> float array
 (**[calc_upvotes encoded_matrix encoded_post] is the predicted number of
-   upvotes for a given post*)
+   upvotes for a given [encoded_post]*)
 
 val graph_results : int array -> float array -> unit
+(**[graph_results predicted_upvotes actual_upvotes] prints out a
+   graphical representation of our prediction algorithm compared to real
+   upvotes*)
